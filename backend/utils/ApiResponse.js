34a2864405b60db3a4aaea87721e940f41b6cb21
Sujal Vasara <vasarasujal.cg@gmail.com>
2026-07-18 16:01:50 +0530

    Standardize backend API response structures
    - Centralized success and error response classes
    - Added async wrapper to prevent try-catch boilerplate

---
+++ backend/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+  constructor(statusCode, data, message = 'Success') {
+    this.success = statusCode < 400;
+    this.message = message;
+    this.data = data;
+  }
+}
+export default ApiResponse;
